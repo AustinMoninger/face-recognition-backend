8b3015d54b241a3e2952f133fcfbbc5eb03e68b6
austinmoninger <austinmoninger@gmail.com>
2019-04-11 15:28:29 -0500

    Add database url

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-curved-91500',
-      user : 'austinmoninger',
-      password : '',
-      database : 'face-recognition'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
